9760d89419db703026862a1de9240d4cede8aeb5
Kevin Quinn <kpquinn2@wisc.edu>
2021-09-01 08:25:29 -0400

    A Second Trie :D

--- trie.py
+++ trie.py
@@ -1,10 +1,62 @@
 
+#########################################################
+#
+# Second Trie ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿
+#
+# Simpler to look at since it uses maps instead of lists for children. 
+# Definitely faster than the potential 26 character check, but since it's bound at 26
+# for each child it does dampen how much improvement can be made.
+#########################################################
+
+class SecondTrieNode:
+    def __init__(self, char, end_of_word=False):
+        self.char = char
+        self.end_of_word = end_of_word
+        # have a map from 'a': TrieNode(char='a'), speedier lookups
+        self.children = {}
+
+class SecondTrie:
+    def __init__(self):
+        super().__init__()
+        self.root_node = SecondTrieNode("")
+
+    def insert(self, word):
+        # starting from root node, gotta add one character at a time
+        current_node = self.root_node
+
+        for char in word:
+            try:
+                current_node = current_node.children[char] 
+            except KeyError:
+                current_node.children[char] = SecondTrieNode(char)
+                current_node = current_node.children[char] 
+
+        current_node.end_of_word = True
+
+    def search(self, word):
+        letters_searched = []
+
+        current_node = self.root_node
+        for char in word:
+            found_match = False
+            try:
+                current_node = current_node.children[char]
+                letters_searched.append(char)
+            except KeyError:
+                # didn't find a child for this char, means word is not in here
+                break
+
+        word_found = (word == ''.join(letters_searched))
+        return word_found, letters_searched
 
 
-## Creating a Trie - > Needs something to hold all of the Nodes, and needs each node to be it's own thing
-## 
 
-class TrieNode:
+#########################################################
+#
+# First Trie ¯\_(ツ)_/¯
+#
+#########################################################
+class FirstTrieNode:
     # end of a word? what if it's the end of multiple words? that makes no sense, then they are the same word yes?
     def __init__(self, char, end_of_word=False):
         self.char = char
@@ -12,12 +64,10 @@ class TrieNode:
         # can have up to 26 children, one for each letter of the alphabet
         self.children = []
 
-
-class Trie:
-
+class FirstTrie:
     def __init__(self):
         super().__init__()
-        self.root_node = TrieNode("")
+        self.root_node = FirstTrieNode("")
     
     def insert(self, word):
         # starting from root node, gotta add one character at a time
@@ -34,7 +84,7 @@ class Trie:
                     break
 
             if not found_match:
-                new_node = TrieNode(char)
+                new_node = FirstTrieNode(char)
                 current_node.children.append(new_node)
                 current_node = new_node
 
@@ -61,18 +111,27 @@ class Trie:
         word_found = (word == ''.join(letters_searched))
         return word_found, letters_searched
 
+
 def test_trie():
     word_list = ['taco', 'dog', 'tank']
+    search_list = ['babe', 'taco', 'superpowers', 'tankatsu', '']
 
-    trie = Trie()
+    trie = FirstTrie()
     for word in word_list:
         trie.insert(word)
 
-    search_list = ['babe', 'taco', 'superpowers', 'tankatsu', '']
     for word in search_list:
         found, letters_searched = trie.search(word)
         print(f'SEARCH:{word} -- {found}:{letters_searched}')
 
+    print('======== Second Trie ==========')
+    second_trie = SecondTrie()
+    for word in word_list:
+        second_trie.insert(word)
+
+    for word in search_list:
+        found, letters_searched = second_trie.search(word)
+        print(f'SEARCH:{word} -- {found}:{letters_searched}')
 
 if __name__ == '__main__':
     test_trie()
